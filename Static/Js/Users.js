d79a64cc0ce3024c592db7130919d76272839af8
Brand131521 <neiver.brand@uniminuto.edu.co>
2023-11-21 12:03:52 -0500

    script para la validacion de parametros minimos para la creacion de contraseña

--- Static/Js/Users.js
+++ Static/Js/Users.js
@@ -10,12 +10,16 @@ $(document).ready(function(){
             method: "POST",
             data: {correo: correo, passwd: passwd},
             success: function(response, textStatus, xhr){
-                if(xhr.status === 200 && response.redirect){
+                if (xhr.status === 200 && response.redirect){
                     window.location.href = response.redirect;
                 } else {
                     alert("Invalid credentials. Please try again.");
+                    $("#passwd").val("");
                 }
-            }            
+            },
+            error: function(xhr, textStatus, errorThrown) {
+                alert("Error: " + errorThrown);
+            }          
         });
     });
 
@@ -28,9 +32,21 @@ $(document).ready(function(){
         var usu_sex = $("#usu_sex").val();
         var usu_correo = $("#usu_correo").val();
         var usu_password = $("#usu_password").val();
+        var usu_confirm_password = $("#usu_confirm_password").val();
+        
+        // Validar la contraseña y su confirmación
+        if (!validarContrasena(usu_password)) {
+            alert("La contraseña no cumple con los requisitos mínimos.");
+            return;
+        }
     
+        if (usu_password !== usu_confirm_password) {
+            alert("Las contraseñas no coinciden. Por favor, inténtelo de nuevo.");
+            return;
+        }
+        
         $.ajax({
-            url: registro_url,  
+            url: registro_url,
             method: "POST",
             data: {
                 usu_nom: usu_nom,
@@ -43,15 +59,27 @@ $(document).ready(function(){
                 usu_password: usu_password
             },
             success: function(response, textStatus, xhr){
-                if(xhr.status === 200 && response.redirect){
+                if (xhr.status === 200 && response.redirect){
                     window.location.href = response.redirect;
                 } else {
-                    alert("Registration failed. Please try again.");
+                    alert("La registración falló. Por favor, inténtelo de nuevo.");
                 }
             }
         });
     });
-});
-
-
-
+    
+            //Parametros minimos para la ceacion de la contraseña
+        function validarContrasena(contrasena) {
+            
+            if (contrasena.length < 8) {
+                return false;
+            }
+            if (!/[A-Z]/.test(contrasena)) {
+                return false;
+            }
+            if (!/[0-9]/.test(contrasena) || !/[a-zA-Z]/.test(contrasena)) {
+                return false;
+            }
+            return true;
+        }
+    });
